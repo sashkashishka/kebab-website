9a100ba5c7662fecc4feea191354f1778cc7e855
sashkashishka <alexandr1696@hotmail.com>
2021-03-19 00:02:59 +0200

    feat: 404 page

--- packages/front/src/pages/404/index.tsx
+++ packages/front/src/pages/404/index.tsx
@@ -1,12 +1,58 @@
 import * as React from 'react';
+import { css } from 'astroturf';
 
-// import Page from 'Components/page';
-// import Container from 'Components/container';
+import { Page } from 'Components/page';
+import {
+  Container,
+  Link,
+  Text,
+  Img,
+} from 'Components/atoms';
 
-const NotFoundPage: React.FC = () => (
-  <div>
-    not found
-  </div>
+import notFoundImg from 'Img/404.png';
+
+const NotFound: React.FC = () => (
+  <Page
+    title="Сторінку не знайдено"
+  >
+    <Container
+      css={css`
+        padding-top: 32px;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+        align-items: center;
+        text-align: center;
+        color: var(--black);
+
+        @media all and (min-width: 768px) {
+          & {
+            text-align: start;
+          }
+        }
+      `}
+    >
+      <Text>
+        Сторінки не доступна, але президент так і запрошує скуштувати смачну шаурму
+        {' '}
+        <Link
+          href="/"
+          css={css`
+            color: var(--accent);
+          `}
+        >
+          тут
+        </Link>
+      </Text>
+      <Img
+        src={notFoundImg}
+        alt="not found"
+        css={css`
+          width: 100%;
+
+        `}
+      />
+    </Container>
+  </Page>
 );
 
-export default NotFoundPage;
+export default NotFound;
